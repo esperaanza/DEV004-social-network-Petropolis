b781edcbbe5aaf3d67be41888fc51f6005057c06
Esperaanza <xcaanda100@gmail.com>
2023-03-14 17:41:30 -0600

    git commit

--- src/onNavigate.js
+++ src/onNavigate.js
@@ -1,11 +1,44 @@
-// aqui exportaras las funciones que necesites
-export const onNavigate = (pathname) => {
-  window.history.pushState(
-    {},
-    pathname,
-    window.location.origin + pathname,
-  );
+import { home } from './components/home.js';
+import { login } from './components/login.js';
+import { register } from './components/register.js';
+
+const root = document.getElementById('root');
+
+const routes = {
+  '/': home,
+  '/login': login,
+  '/register': register,
+
 };
+const onNavigate = (pathname, paramRoutes = routes) => {
+  window.history.pushState({}, pathname, window.location.origin + pathname);
+  return root.replaceChildren(paramRoutes[pathname]());
+  // root.removeChild(root.firstChild);
+  // root.appendChild(paramRoutes[pathname]);
+};
+const component = routes[window.location.pathname];
+window.onpopstate = () => {
+  root.removeChild(root.firstChild);
+  root.appendChild(component(onNavigate));
+};
+
+root.appendChild(component(onNavigate));
+
+// // aqui exportaras las funciones que necesites
+// const routes = {
+//   '/': home,
+//   '/register': register,
+// };
+// const rootDiv = document.getElementById('root');
+
+// export const onNavigate = (pathname) => {
+//   window.history.pushState(
+//     {},
+//     pathname,
+//     window.location.origin + pathname,
+//   );
+//   rootDiv.appendChild(routes[pathname]);
+// };
 
 // *Este es el codigo del ejemplo del video min :36
 // export const onNavigate = (pathname) => {
@@ -35,4 +68,5 @@ export const onNavigate = (pathname) => {
 //   );
 // };
 
+// eslint-disable-next-line no-console
 console.log('Hola mundo!');
